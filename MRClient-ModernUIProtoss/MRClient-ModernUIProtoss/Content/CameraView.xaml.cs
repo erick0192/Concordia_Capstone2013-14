d706faa108e75fdd4459019a48a96be022184002
jpadillak <jpadillak@gmail.com>
2013-09-23 11:40:01 -0400

    Preliminary Client Prototype

---
+++ MRClient-ModernUIProtoss/MRClient-ModernUIProtoss/Content/CameraView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MRClient_ModernUIProtoss.Content
+{    
+    /// <summary>
+    /// Interaction logic for CameraView.xaml
+    /// </summary>
+    public partial class CameraView : UserControl
+    {
+        public string Heading { get; set; }
+        private bool mIsActive;
+        public bool IsActive 
+        { 
+            get { return mIsActive; }
+            set
+            {
+                mIsActive = value;
+                if (mIsActive)
+                {
+                    
+                }
+            }
+        }
+
+        public CameraView() : this("View")
+        {
+            
+        }
+
+        public CameraView(string iHeading)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Heading = iHeading;
+            IsActive = true;
+        }
+    }
+}
---
+++ MRClient-ModernUIProtoss/MRClient-ModernUIProtoss/Pages/Main.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MRClient_ModernUIProtoss.Content;
+
+namespace MRClient_ModernUIProtoss.Pages
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : UserControl
+    {
+        public Main()
+        {
+            InitializeComponent();
+            InitKeyBindings();
+        }
+
+        public void InitKeyBindings()
+        {
+            KeyBinding OpenCmdKeyBinding = new KeyBinding(
+            ApplicationCommands.Open,
+            Key.R,
+            ModifierKeys.Control);
+
+            this.InputBindings.Add(OpenCmdKeyBinding);
+        }
+    }
+}
