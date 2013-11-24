b84f3a4238e49dd9c3a02ff44823f41c9eb2ea66
Alex <alex.teodor@spaceconcordia.ca>
2013-11-24 14:06:47 -0500

    Send part complete, receive part almost finished

--- MarsRover/Prototype & Tests/TCP vs UDP commands/UDP Receive/UDP Receive/Program.cs
+++ MarsRover/Prototype & Tests/TCP vs UDP commands/UDP Receive/UDP Receive/Program.cs
@@ -2,13 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace UDP_Receive
 {
+    /// <summary>
+    /// This UDP server receives a fake command. The fake command is sent from the UDP client at fixed intervals. The value of those intervals are held within the commmand.
+    /// </summary>
     class Program
     {
+        static UdpClient udpServer;
+        static int packets_received;
+        static int timerInterval;
+        static IPEndPoint udpEp;
+        static bool receiveThreadContinue;
+        static int commandSize;
         static void Main(string[] args)
         {
+            receiveThreadContinue = true;
+            int port = 5000;
+            udpEp = new IPEndPoint(IPAddress.Any, port);
+
+            
+            
+            //----------------------------------
+            udpServer = new UdpClient(port);
+
+            //----------------------------------
+            Thread receiveThread = new Thread(new ThreadStart(receiver));
+            receiveThread.Start();
+
+            ///----------------------------------
+
+            Console.WriteLine("Press Enter to stop and see some stats\n");
+            Console.ReadLine();
+            receiveThreadContinue = false;
+
+            Console.WriteLine("Packets received: {0}\nTimerInterval: {1}\nCommand size: {2}", packets_received, timerInterval, commandSize);
+            Console.WriteLine("Press Enter to exit\n");
+            Console.ReadLine();
+            return;
+        }
+
+        static void receiver()
+        {
+            byte[] receivedData = new byte[1000];
+            while (receiveThreadContinue == true)
+            {
+                receivedData = udpServer.Receive(ref udpEp);
+                commandSize = receivedData.Length;
+                timerInterval = BitConverter.ToInt32(receivedData, 0);
+                packets_received++;
+            }
         }
     }
 }
--- MarsRover/Prototype & Tests/TCP vs UDP commands/UDP Send/UDP Send/Program.cs
+++ MarsRover/Prototype & Tests/TCP vs UDP commands/UDP Send/UDP Send/Program.cs
@@ -3,40 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Timers;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UDP_Send
 {
+    /// <summary>
+    /// This UDP client sends a frame containing a pseudo-command to a UDP server. The command is sent using a timer. The command contains the value of the timer, not an actual command. 
+    /// </summary>
     class Program
     {
         static byte[] command;
+        static int packet_sent_count;
+        static UdpClient udpClient;
+
         static void Main(string[] args)
         {
-            double timerInterval = 1000;
+            int timerInterval;
+            int commandSize;
+            string serverIP;
+            int port;
+
+            if (args.Length != 4)
+            {
+                Console.WriteLine("What're you doing? These are the arguments that need be passed:");
+                Console.WriteLine("1. Timer interval (int)");
+                Console.WriteLine("2. Command size in bytes (int)");
+                Console.WriteLine("3. IP address of the server (string)");
+                Console.WriteLine("4. Port to connect to (int)");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                timerInterval = int.Parse(args[0]);
+                commandSize = int.Parse(args[1]);
+                serverIP = args[2];
+                port = int.Parse(args[3]);
+            }
             
 
+            //keeping track of how many commands over UDP have been sent
+            packet_sent_count = 0;
+
             //set the command size in terms of bytes
-            int commandSize = 20;
             command = new byte[commandSize];
 
             //filling up the command with the value of the timer interval instead of an actual command;
             //good way to tell the receiving end what our interval is on this side
-            Array.Copy(BitConverter.GetBytes(commandSize), command, BitConverter.GetBytes(commandSize).Length);
+            Array.Copy(BitConverter.GetBytes(timerInterval), command, BitConverter.GetBytes(commandSize).Length);
 
+            //---------------------
             //setting up the timer
-            Timer tim = new Timer(timerInterval);
+            Timer tim = new Timer((double)timerInterval);
             tim.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             tim.Start();
-            
-            
-            
-            while(true);
-            
+            //---------------------
+            //setting up UDP client; UDP client sends to server
+            udpClient = new UdpClient(serverIP, port);
+
+
+
+            //---------------------
+
+            Console.WriteLine("Press enter to stop the sending and see some stats\n");
+            Console.ReadLine();
+            tim.Stop();
+            Console.WriteLine("Packets sent:{0}\nTimer interval (ms):{1}\nCommand size (bytes):{2}\n", packet_sent_count, timerInterval, commandSize);
+
+
+            Console.WriteLine("Press enter again to exit");
+            Console.ReadLine();
+
         }
 
         static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("time up");
+            //keep counting how many packets are sent
+            packet_sent_count++;
             
+            //send the ducking thing
+            udpClient.Send(command, command.Length);
         }
+
+        
     }
 }
