7a49b7b8d036eb457c02134109d3157bda0167f2
Alex <alex.teodor@spaceconcordia.ca>
2013-11-24 02:17:00 -0500

    Code to test the performance of TCP vs UDP to send commands at long distance

---
+++ MarsRover/Prototype & Tests/TCP vs UDP commands/TCP Receive/TCP Receive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCP_Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
