ae9e7da4d607da16c9d34905e1e7a25512630725
BBackerry <falah.salim@gmail.com>
2013-12-16 02:32:36 -0500

    Integrated MovementCommand with GamepadController

--- MarsRover/MarsRoverClient/Gamepad/GamepadController.cs
+++ MarsRover/MarsRoverClient/Gamepad/GamepadController.cs
@@ -4,20 +4,19 @@ using NLog;
 using SharpDX;
 using SharpDX.XInput;
 using System.Collections.Generic;
+using System.Text;
 
 namespace MarsRoverClient.Gamepad
 {
     class GamepadController
     {
-        //Straight up from SharpDX docs
         private Logger logger = LogManager.GetCurrentClassLogger();
         private const int POLLING_RATE = 100; //milliseconds
-        private const int LEFT_THUMB_DEADZONE = 7849;
 
         public GamepadController()
         {
+            //TODO: Detect new controllers after start ?
             var controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
-
             StartPollingAndSendingCommands(controllers);
         }
 
@@ -27,7 +26,7 @@ namespace MarsRoverClient.Gamepad
             {
                 if (controller.IsConnected)
                 {
-                    logger.Debug("Starting thread for Controller {0}", controller.GetHashCode());
+                    logger.Debug("Found a XInput controller available");
                     Thread t = new Thread(() => Poll(controller));
                     t.Start();
                 }
@@ -36,46 +35,37 @@ namespace MarsRoverClient.Gamepad
 
         private void Poll(Controller controller)
         {
-            logger.Debug("Found a XInput controller available");
-
             var previousState = controller.GetState();
             while (controller.IsConnected)
             {
                 var state = controller.GetState();
-                if (previousState.PacketNumber != state.PacketNumber) {
-                    //http://msdn.microsoft.com/en-us/library/windows/desktop/ee417001(v=vs.85).aspx#dead_zone
-                    int LX = state.Gamepad.LeftThumbX;
-                    int LY = state.Gamepad.LeftThumbY;
+                double LX = state.Gamepad.LeftThumbX;
+                double LY = state.Gamepad.LeftThumbY;
+                double RX = state.Gamepad.RightThumbX;
+                double RY = state.Gamepad.RightThumbY;
 
-                    double magnitude = Math.Sqrt(LX * LX + LY * LY);
-                    double normalizedLX = LX / magnitude;
-                    double normalizedLY = LY / magnitude;
-                    double normalizedMagnitude = 0;
+                StringBuilder command = new StringBuilder();
+                command.Append("<");
 
-                    if (magnitude > LEFT_THUMB_DEADZONE)
-                    {
-                        if (magnitude > 32767)
-                            magnitude = 32767;
-                        normalizedMagnitude = magnitude / (32767 - LEFT_THUMB_DEADZONE);
-                    }
-                    else
-                    {
-                        magnitude = 0.0;
-                        normalizedMagnitude = 0.0;
-                    }
+                string leftDirection = LY > 0 ? "F" : "B";
+                command.Append(leftDirection);
+                string leftSpeed = (int)Math.Abs((LY * 255 / 32768)) + "";
+                command.Append(leftSpeed);
 
-                    if (normalizedMagnitude > 0) //For sensitive gamepads
-                    {
-                        string xCommand = normalizedLX > 0 ? "L" : "R";
-                        string yCommand = normalizedLY > 0 ? "W" : "S";
-                        //Speed is normalizedMagnitude in [0,1]
-                        logger.Debug("Go " + xCommand + " and " + yCommand); //Send by TCP instead
-                    }
+                string rightDirection = RY > 0 ? "F" : "B";
+                command.Append(rightDirection);
+                string rightSpeed = (int)Math.Abs((RY * 255 / 32768)) + "";
+                command.Append(rightSpeed);
 
-                    previousState = state;
-                }
+                command.Append(">");
+                
+                //logger.Debug(command);
+                //TODO: Send command
+
+                previousState = state;
                 Thread.Sleep(POLLING_RATE);
             }
         }
+
     }
 }
