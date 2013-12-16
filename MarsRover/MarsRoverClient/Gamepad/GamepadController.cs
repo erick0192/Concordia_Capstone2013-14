8d25e4749e50ca7f562669ccfc023f558363d376
BBackerry <falah.salim@gmail.com>
2013-12-16 01:20:02 -0500

    Gamepad Controller
    (Not sending commands yet)

--- MarsRover/MarsRoverClient/Gamepad/GamepadController.cs
+++ MarsRover/MarsRoverClient/Gamepad/GamepadController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading;
 using NLog;
-using SharpDX.DirectInput;
+using SharpDX;
+using SharpDX.XInput;
 using System.Collections.Generic;
 
 namespace MarsRoverClient.Gamepad
@@ -8,42 +10,71 @@ namespace MarsRoverClient.Gamepad
     class GamepadController
     {
         //Straight up from SharpDX docs
-        private DirectInput directInput;
-        private List<Joystick> gamepads;
-        private Logger logger;
+        private Logger logger = LogManager.GetCurrentClassLogger();
+        private const int POLLING_RATE = 100; //milliseconds
+        private const int LEFT_THUMB_DEADZONE = 7849;
 
         public GamepadController()
         {
-            logger = LogManager.GetCurrentClassLogger();
-            directInput = new DirectInput();
-            gamepads = new List<Joystick>(2);
+            var controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
 
-            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+            StartPollingAndSendingCommands(controllers);
+        }
+
+        public void StartPollingAndSendingCommands(Controller[] controllers)
+        {
+            foreach (var controller in controllers)
             {
-                var gamepad = new Joystick(directInput, deviceInstance.InstanceGuid);
-                gamepad.Properties.BufferSize = 128;
-                gamepads.Add(gamepad);
-                logger.Trace("Added Gamepad with Guid: " + deviceInstance.InstanceGuid);
+                if (controller.IsConnected)
+                {
+                    logger.Debug("Starting thread for Controller {0}", controller.GetHashCode());
+                    Thread t = new Thread(() => Poll(controller));
+                    t.Start();
+                }
             }
         }
 
-        public void StartPollingAndSendingCommands()
+        private void Poll(Controller controller)
         {
-            foreach (var gp in gamepads)
-            {
-                gp.Acquire();
-            }
+            logger.Debug("Found a XInput controller available");
 
-            //TODO: Start in a thread for each gamepad
-            while (true)
+            var previousState = controller.GetState();
+            while (controller.IsConnected)
             {
-                foreach (var gp in gamepads)
-                {
-                    gp.Poll();
-                    var datas = gp.GetBufferedData();
-                    foreach (var state in datas)
-                        logger.Trace(state);
+                var state = controller.GetState();
+                if (previousState.PacketNumber != state.PacketNumber) {
+                    //http://msdn.microsoft.com/en-us/library/windows/desktop/ee417001(v=vs.85).aspx#dead_zone
+                    int LX = state.Gamepad.LeftThumbX;
+                    int LY = state.Gamepad.LeftThumbY;
+
+                    double magnitude = Math.Sqrt(LX * LX + LY * LY);
+                    double normalizedLX = LX / magnitude;
+                    double normalizedLY = LY / magnitude;
+                    double normalizedMagnitude = 0;
+
+                    if (magnitude > LEFT_THUMB_DEADZONE)
+                    {
+                        if (magnitude > 32767)
+                            magnitude = 32767;
+                        normalizedMagnitude = magnitude / (32767 - LEFT_THUMB_DEADZONE);
+                    }
+                    else
+                    {
+                        magnitude = 0.0;
+                        normalizedMagnitude = 0.0;
+                    }
+
+                    if (normalizedMagnitude > 0) //For sensitive gamepads
+                    {
+                        string xCommand = normalizedLX > 0 ? "L" : "R";
+                        string yCommand = normalizedLY > 0 ? "W" : "S";
+                        //Speed is normalizedMagnitude in [0,1]
+                        logger.Debug("Go " + xCommand + " and " + yCommand); //Send by TCP instead
+                    }
+
+                    previousState = state;
                 }
+                Thread.Sleep(POLLING_RATE);
             }
         }
     }
