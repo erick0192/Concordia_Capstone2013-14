1cb6a2ab925f11351ba94b78d900665d9f069cb3
Matthological <matthological@gmail.com>
2014-01-23 18:30:54 -0500

    Preliminary GPS class used for receiving data from the microcontroller.

---
+++ MarsRover/Rover/ISensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rover
+{
+    public interface ISensorLog //change RawCommand to RawData.
+    {
+        string RawCommand { get; }
+        bool IsUpdated { get; }
+
+        void LogData();
+        void UpdateValues();
+        void ReconstructCommand();
+    }
+}
