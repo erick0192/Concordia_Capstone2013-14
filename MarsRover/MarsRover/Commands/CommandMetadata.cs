66f57cb87f8f812af843dc3e8b1bc13c924cba15
jpadillak <jpadillak@gmail.com>
2014-01-23 17:54:20 -0500

    Moved CommandMetadata to MarsRover lib

---
+++ MarsRover/MarsRover/Commands/CommandMetadata.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarsRover.Commands
+{
+    static class CommandMetadata
+    {
+        public const int IdIndex = 1;
+        public const int IdLength = 1; //Perhaps we will have to set the ID length to 2 in the future.
+        public const string StartDelimiter = "<";
+        public const string EndDelimiter = ">";
+
+        public static class Movement
+        {
+            //Example of assumed movement command format: <MF255F255>
+
+            public const string Identifier = "M";
+            public const string Forward = "F";
+            public const string Backward = "B";
+
+            public const int MaxSpeed = 255;
+
+            public const int DirectionLength = 1;
+            public const int IdentifierLength = 1;
+
+            public const int LeftDirectionIndex = 2;
+            public const int RightDirectionIndex = 6;
+
+            public const int LeftSpeedStartIndex = 3;
+            public const int RightSpeedStartIndex = 7;
+
+            public const int LeftSpeedEndIndex = 5;
+            public const int RightSpeedEndIndex = 9;
+        }
+
+        public static class Camera
+        {
+            //Example cam on/off command: <C1F>
+            public const string Identifier = "C";
+            public const string On = "O";
+            public const string Off = "F";
+
+            public const int NumberIndex = 2;
+            public const int NumberLength = 1;
+            public const int StatusIndex = 3;
+            public const int StatusLength = 1;
+        }
+
+        public static class Pan
+        {
+            //Example command: <P0359>
+            public const int MaxPanAngle = 359;
+            public const int MinPanAngle = 0;
+
+            public const int NumberIdentifierIndex = 2;
+            public const int NumberIdentifierLength = 1;
+
+            public const int AngleStartIndex = 3;
+            public const int AngleEndIndex = 5;
+        }
+
+        public static class Tilt
+        {
+            //Example command: <T1000>
+            public const int MaxTiltAngle = 90;
+            public const int MinTiltAngle = 0;
+
+            public const int NumberIdentifierIndex = 2;
+            public const int NumberIdentifierLength = 1;
+
+            public const int AngleStartIndex = 3;
+            public const int AngleEndIndex = 5;
+
+
+        }
+    }
+}
