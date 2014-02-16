8f7bf9aa79f074a83cb4de40eb6dd28591037164
donalonse <donalonse@gmail.com>
2014-02-16 15:20:52 -0500

    WatchDog for the On-Board Computer and its associated Unit Tests

---
+++ MarsRover/MarsRover/WatchDog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MarsRover
+{
+    public class WatchDog
+    {
+        #region Attributes
+
+        private const double BASE_TIME = 3.0f;
+
+        private Stopwatch _stopwatch;
+
+        #endregion
+
+        #region Constructors
+
+        public WatchDog()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void reportActivity()
+        {
+            _stopwatch.Restart();
+        }
+
+        public bool allowCommand()
+        {
+            return (BASE_TIME > _stopwatch.ElapsedMilliseconds / 1000.0);
+        }
+
+        #endregion
+    }
+}
---
+++ MarsRover/MarsRoverTest/WatchDogTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using MarsRover;
+
+namespace MarsRoverTest
+{
+    class WatchDogTest
+    {
+        MarsRover.WatchDog wd;
+
+        [Test]
+        public void allowCommand_NotTimedOut_ReturnTrue()
+        {
+            wd = new MarsRover.WatchDog();
+            wd.reportActivity();
+
+            Assert.AreEqual(wd.allowCommand(), true);
+        }
+
+        [Test]
+        public void reportActivity_NotTimedOut_AllowCommandAlwaysTrue()
+        {
+            int delay = 500;
+            wd = new MarsRover.WatchDog();
+
+            int i;
+            for (i = 0; i < 10000; i += delay)
+            {
+                Thread.Sleep(delay);
+                wd.reportActivity();
+
+                Assert.AreEqual(wd.allowCommand(), true);
+            }
+        }
+
+        [Test]
+        public void allowCommand_TimedOut_ReturnFalse()
+        {
+            wd = new MarsRover.WatchDog();
+            Thread.Sleep(5000);
+
+            Assert.AreEqual(wd.allowCommand(), false);
+        }
+
+        [Test]
+        public void reportActivity_TimedOut_AllowComandBecomesTrue()
+        {
+            wd = new MarsRover.WatchDog();
+            Thread.Sleep(5000);
+
+            bool before = wd.allowCommand();
+            wd.reportActivity();
+            bool after = wd.allowCommand();
+
+            Assert.AreNotEqual(before, after);
+            Assert.AreEqual(after, true);
+        }
+    }
+}
