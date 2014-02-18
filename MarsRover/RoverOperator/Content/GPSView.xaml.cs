5bc2893b574249cd8289f721477f2f450f9efdcb
m_barrym <michael.barry.mancuso@gmail.com>
2014-02-17 19:09:41 -0500

    Current Working Features:
    + View map
    + Navigate map with mouse (zoom, pan, etc.. all built-in features of Bing Maps control)
    + Add target pins on double-click
    + Remove target pins on right-click
    + Add Target pins by entering longitude and latitude manually
    
    Features to be implemented:
    - Display rover location
    - Auto-update rover location
    - Caching maps
    - Smoother input validation when creating Targets (enforce proper longitude, latitude format on UI level)

--- MarsRover/RoverOperator/Content/GPSView.xaml.cs
+++ MarsRover/RoverOperator/Content/GPSView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
 
 namespace RoverOperator.Content
 {
@@ -20,10 +21,174 @@ namespace RoverOperator.Content
     /// </summary>
     public partial class GPSView : UserControl
     {
+        GPSViewViewModel gpsVM;
+
+        //keep track of targets on map
+        private List<Pushpin> targetPins;
+
         public GPSView()
         {
             InitializeComponent();
-            DataContext = new GPSViewModel();
+
+            gpsVM = new GPSViewViewModel();
+
+            //bind events
+            myMap.MouseDoubleClick += map_MouseDoubleClick;
+
+            DataContext = this;
+        }
+
+        //handle mouse click to add targets
+        private void map_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Disables the default mouse double-click action.
+            e.Handled = true;
+
+            // Determin the location to place the pushpin at on the map.
+
+            //Get the mouse click coordinates
+            Point mousePosition = e.GetPosition(this);
+            //Convert the mouse coordinates to a locatoin on the map
+            Location targetLocation = myMap.ViewportPointToLocation(mousePosition);
+            
+            addTarget(targetLocation);           
+        }
+
+        //Add a target to the map from the text boxes
+        private void addTarget(Location location)
+        {
+            //Initialize target list
+            if (targetPins == null)
+            {
+                targetPins = new List<Pushpin>();
+            }
+
+            Location targetLocation = location;
+
+            // The pushpin to add to the map.
+            Pushpin targetPin = new Pushpin();
+            targetPin.Location = targetLocation;
+            
+            //Add a tooltip to the pin
+            ToolTip tt = new ToolTip();
+            tt.Content = targetPin.Location.Longitude + ", " + targetPin.Location.Latitude;
+            targetPin.ToolTip = tt;
+
+            //remove target on rightclick event handler
+            targetPin.MouseRightButtonUp += targetPin_MouseRightButtonUp;
+
+            // Adds the pushpin to the map.
+            myMap.Children.Add(targetPin);
+
+            //Initialize target list
+            if (targetPins == null)
+            {
+                targetPins = new List<Pushpin>();
+            }
+            targetPins.Add(targetPin);
+        }
+
+        /// <summary>
+        /// Remove target on right click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void targetPin_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            myMap.Children.Remove((UIElement) sender);
+            targetPins.Remove((Pushpin)sender);
+        }
+
+        //when you click the 'Remove' button
+        private void removeTarget()
+        {
+            //check both but they should be the same number all the time
+            if (myMap.Children.Count > 0 && targetPins.Count > 0)
+            {
+                myMap.Children.Remove(targetPins.Last());
+                targetPins.Remove(targetPins.Last());
+            }
+        }
+
+        //when you click the 'Remove All' button
+        private void removeAllTargets()
+        {
+            //check both but they should be the same number all the time
+            if (myMap.Children.Count > 0 && targetPins.Count > 0)
+            {
+                foreach (var t in targetPins)
+                {
+                    myMap.Children.Remove(t);
+                    targetPins.Remove(t);
+                }
+            }
+        }
+
+        //Add a marker to the map
+        private Ellipse placeGPSPoint(Location location, string name, Color color)
+        {
+            Ellipse target = new Ellipse();
+
+            target.Fill = new SolidColorBrush(color);
+            double radius = 12.0;
+            target.Width = radius * 2;
+            target.Height = radius * 2;
+            ToolTip tt = new ToolTip();
+            tt.Content = name + ": " + location;
+            target.ToolTip = tt;
+            Point p0 = myMap.LocationToViewportPoint(location);
+            Point p1 = new Point(p0.X - radius, p0.Y - radius);
+            Location loc = myMap.ViewportPointToLocation(p1);
+            MapLayer.SetPosition(target, loc);
+            myMap.Children.Add(target);
+
+            return target;
+        }
+
+        private void removeTargetButton_Click(object sender, RoutedEventArgs e)
+        {
+            removeTarget();
+        }
+
+        private void removeAllTargetsButton_Click(object sender, RoutedEventArgs e)
+        {
+            removeAllTargets();
+        }
+
+        private void addTargetButton_Click(object sender, RoutedEventArgs e)
+        {
+            Location targetLocation = new Location();
+
+            double longitude;
+            double latitude;
+
+            bool resultLong = (Double.TryParse(longitudeBox.Text, out longitude));
+            bool resultLat = (Double.TryParse(latitudeBox.Text, out latitude));
+
+            if (resultLong && resultLat)
+            {
+                
+                //latitude ranges from -90 to 90 
+                targetLocation.Latitude = latitude % 90;
+
+                //longitude ranges from -180 to 180
+                targetLocation.Longitude = longitude % 180;
+
+                addTarget(targetLocation);
+            }
+            else
+            {
+                if (!resultLong)
+                {
+                    Console.Out.WriteLine("Invalid longitude entered.");
+                    longitudeBox.Clear();
+                }
+                if (!resultLat)
+                {
+                    Console.Out.WriteLine("Invalid latitude entered.");
+                    latitudeBox.Clear();
+                }
+            }
         }
     }
 }
---
+++ MarsRover/RoverOperator/Content/GPSViewViewModel.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maps.MapControl.WPF;
+using MarsRover;
+
+namespace RoverOperator.Content
+{
+    class GPSViewViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public String Title
+        {
+            get
+            {
+                return "Location"; //Temporary
+            }
+
+        }
+
+        private GPSMap gpsMap;
+
+        private GPSCoordinates roverCoordinates;
+        public GPSCoordinates RoverCoordinates
+        {
+            get
+            {
+                return roverCoordinates;
+            }
+        }
+
+        private List<GPSLocator> targetCoordinates;
+
+        #endregion
+
+        #region Delegates and Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Constructor
+
+        public GPSViewViewModel()
+        {
+            StatusUpdater.Instance.GPSCoordinatesUpdated += new StatusUpdater.GPSCoordinatesUpdatedDelegate(UpdateGPS);
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        //Updated coordinates from status update
+        private void UpdateGPS(GPSCoordinates gpsCoordinates)
+        {
+            roverCoordinates = gpsCoordinates;
+            
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("RoverCoordinates"));
+            }
+        }      
+
+        //User adds new target to map
+        private void addTarget(double targetLatitude, double targetLongitude)
+        {
+            if (targetCoordinates == null)
+            {
+                targetCoordinates = new List<GPSLocator>();
+            }
+
+            targetCoordinates.Add(new GPSLocator(targetLatitude, targetLongitude));
+        }
+
+        #endregion
+
+        
+    }
+
+    #region Class Definitions
+    /// <summary>
+    /// Class which represents the map itself
+    /// </summary>
+    class GPSMap
+    {
+        #region Properties
+        private string mapImageURL;
+        private double minLongitude, maxLongitude, minLatitude, maxLatitude;
+        #endregion
+
+        #region Constructors
+        public GPSMap(string mapImageURL)
+        {
+            this.mapImageURL = mapImageURL;
+        }
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Class used to define targets on the map (non-rover coordinates)
+    /// </summary>
+    class GPSLocator
+    {
+        #region Properties
+        private double longitude, latitude;
+        public double Longitude
+        {
+            get
+            {
+                return longitude;
+            }
+        }
+
+        public double Latitude
+        {
+            get
+            {
+                return latitude;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public GPSLocator(double latitude, double longitude)
+        {
+            this.longitude = latitude;
+            this.latitude = longitude;
+        }
+
+        public GPSLocator(GPSCoordinates gpsCoordinates)
+        {
+            //parse longitude and latitude from gpsCoordinates
+            this.longitude = nmeaToLongitude(gpsCoordinates.X, gpsCoordinates.Y);
+            this.latitude = nmeaToLatitude(gpsCoordinates.Z);
+        }
+        #endregion
+
+        #region Methods
+        //NMEA in X, Y, Z format methods for longitude and latitude
+        private double nmeaToLatitude(double Z)
+        {
+            return Math.Asin(Z / 6371); //6371 is approximate radius of the earth in km
+        }
+        private double nmeaToLongitude(double X, double Y)
+        {
+            return Math.Atan2(Y, X);
+        }
+
+        //Raw NMEA string parse method to extract longitude or latitude in decimal form
+        private double parseNmea(string nmeaCoordinates)
+        {
+            double decimalCoordinates = 0;
+
+            string degreeString;
+            string minuteString;
+
+            try
+            {
+
+                if (nmeaCoordinates.Contains('.'))
+                {
+                    if (nmeaCoordinates.Split('.').Length > 2)
+                    {
+                        //error
+                    }
+
+                    try
+                    {
+                        //get degrees
+                        degreeString = nmeaCoordinates.Substring(0, nmeaCoordinates.IndexOf('.') - 3); //split at decimal point, select degree representing characters
+                    }
+                    catch
+                    {
+                        degreeString = "0"; // 0 degrees is a rare case
+                    }
+
+                    try
+                    {
+                        minuteString = nmeaCoordinates.Substring(nmeaCoordinates.IndexOf('.') - 2); // select last 2 characters before the decimal point until the end of the string, which represent minutes (if there are 2 digits of minutes)
+                    }
+                    catch
+                    {
+                        minuteString = nmeaCoordinates.Substring(nmeaCoordinates.IndexOf('.') - 1); // if there are 1 digit of minutes (rare case)
+                    }
+
+                    decimalCoordinates = Convert.ToDouble(degreeString) + (Convert.ToDouble(minuteString) / 60); // divide minutes by 60 to give degrees
+                }
+                else
+                {
+                    if (nmeaCoordinates.Length > 2)
+                    {
+                        //get degrees
+                        degreeString = nmeaCoordinates.Remove(nmeaCoordinates.Length - 2); //remove last 2 characters which represent whole minutes
+
+                        // get minutes
+                        minuteString = nmeaCoordinates.Substring(nmeaCoordinates.Length - 2); // select last 2 characters which represent minutes
+
+                        decimalCoordinates = Convert.ToDouble(degreeString) + (Convert.ToDouble(minuteString) / 60); // divide minutes by 60 to give degrees
+                    }
+                    else
+                    {
+                        //last 2 characters are always minutes, therefore we don't have degrees
+                        minuteString = nmeaCoordinates;
+                        degreeString = "0";
+
+                        decimalCoordinates = Convert.ToDouble(minuteString) / 60; // divide minutes by 60 to give degrees
+                    }
+                }
+            }
+            catch
+            {
+                //something went wrong
+                decimalCoordinates = 0;
+            }
+
+            return decimalCoordinates;
+        }
+
+        #endregion
+    }
+     
+    #endregion
+}
