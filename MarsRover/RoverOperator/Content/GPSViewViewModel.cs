1f7e5431dbe373c9f93c69a8dbb51186ed58130d
m_barrym <michael.barry.mancuso@gmail.com>
2014-03-09 21:27:14 -0400

    Re-factor entire GPS module from MVC to MVVM. Add style to rover and target pins.

--- MarsRover/RoverOperator/Content/GPSViewViewModel.cs
+++ MarsRover/RoverOperator/Content/GPSViewViewModel.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Maps.MapControl.WPF;
 using MarsRover;
+using System.Windows.Controls;
 
 namespace RoverOperator.Content
 {
@@ -23,10 +23,157 @@ namespace RoverOperator.Content
 
         }
 
-        public GPSCoordinates roverCoordinates { get; set; }
-        public string roverCoordinateString { get; set; }
+        private Pushpin _roverPin;
+        public Pushpin roverPin 
+        {
+            get
+            {               
+                return _roverPin;
+            }
+            set
+            {
+                _roverPin = value;
+                if (roverPin != null)
+                {
+                    _roverPin.Visibility = System.Windows.Visibility.Visible;
 
-        private List<GPSLocator> targetCoordinates;
+                    //If the rover pin is not already in the pushpin collection, add it
+                    if (!pushPinCollection.Contains(roverPin))
+                    {
+                        pushPinCollection.Add(roverPin);
+                    }
+                }
+                else
+                {
+                    _roverPin.Visibility = System.Windows.Visibility.Hidden;
+                }
+                OnPropertyChanged("roverPin");
+                //notify that the collection has updates
+                OnPropertyChanged("pushPinCollection");
+            }
+        }
+
+        private GPSCoordinates _roverCoordinates;
+        public GPSCoordinates roverCoordinates
+        {
+            get
+            {
+                return _roverCoordinates;
+            }
+            set
+            {
+                _roverCoordinates = value;                
+                OnPropertyChanged("roverCoordinates");
+            }
+        }
+
+        private string _roverCoordinateString;
+        public string roverCoordinateString
+        {
+            get
+            {
+                return _roverCoordinateString;
+            }
+            set
+            {
+                _roverCoordinateString = value;                
+                OnPropertyChanged("roverCoordinateString");
+            }
+        }
+
+        //keep track of targets on map
+        private List<Pushpin> targetPins;
+        private ObservableCollection<Pushpin> _pushPinCollection;
+        public ObservableCollection<Pushpin> pushPinCollection
+        {
+            get
+            {
+                return _pushPinCollection;
+            }
+            set
+            {
+                _pushPinCollection = value;
+                OnPropertyChanged("pushPinCollection");
+            }
+        }
+
+
+        //for input validation of longitude and latitude
+        private string oldLongitudeString = "";
+        private string oldLatitudeString = "";
+
+        /// <summary>
+        /// Coordinate values of the new target (populated by longitude and latitude text boxes)
+        /// </summary>
+        private string _longitudeString;
+        public string longitudeString
+        {
+            get
+            {
+                return _longitudeString;
+            }
+            set
+            {
+                _longitudeString = value;
+                //If the text entered by the user is accepted, store it, else reset it to the stored value
+                if (validateCoordinateString(_longitudeString, out longitude))
+                {                    
+                    oldLongitudeString = _longitudeString;
+                }
+                else
+                {
+                    _longitudeString = oldLongitudeString;
+                }
+
+                determineCanAddTarget();
+                
+                OnPropertyChanged("longitudeString");
+            }
+        }
+        private string _latitudeString;
+        public string latitudeString
+        {
+            get
+            {
+                return _latitudeString;
+            }
+            set
+            {
+                _latitudeString = value;
+                //If the text entered by the user is accepted, store it, else reset it to the stored value
+                if (validateCoordinateString(_latitudeString, out latitude))
+                {                    
+                    oldLatitudeString = _latitudeString;
+                }
+                else
+                {
+                    _latitudeString = oldLatitudeString;
+                }
+
+                determineCanAddTarget();
+
+                OnPropertyChanged("latitudeString");
+            }
+        }
+
+        //double values used for adding targets
+        private double longitude = 0;
+        private double latitude = 0;
+
+        //A name that the user assigns to a target
+        private string _targetTitleString;
+        public string targetTitleString
+        {
+            get
+            {
+                return _targetTitleString;
+            }
+            set
+            {
+                _targetTitleString = value;
+                OnPropertyChanged("targetTitleString");
+            }
+        }
 
         #endregion
 
@@ -37,12 +184,10 @@ namespace RoverOperator.Content
         //Updated coordinates from status update
         private void UpdateGPS(GPSCoordinates gpsCoordinates)
         {
-            roverCoordinates = gpsCoordinates;
+            roverCoordinates = gpsCoordinates;            
             roverCoordinateString = "Rover: " + gpsCoordinates.Location.ToString();
-            
-            OnPropertyChanged("roverCoordinates");
-            OnPropertyChanged("roverCoordinateString");
         }
+      
 
         // Create the OnPropertyChanged method to raise the event 
         protected void OnPropertyChanged(string name)
@@ -52,18 +197,7 @@ namespace RoverOperator.Content
             {
                 handler(this, new PropertyChangedEventArgs(name));
             }
-        }
-
-        //User adds new target to map
-        private void addTarget(double targetLatitude, double targetLongitude)
-        {
-            if (targetCoordinates == null)
-            {
-                targetCoordinates = new List<GPSLocator>();
-            }
-
-            targetCoordinates.Add(new GPSLocator(targetLatitude, targetLongitude));
-        }
+        }       
 
         #endregion
 
@@ -72,11 +206,296 @@ namespace RoverOperator.Content
         public GPSViewViewModel()
         {
             StatusUpdater.Instance.GPSCoordinatesUpdated += new StatusUpdater.GPSCoordinatesUpdatedDelegate(UpdateGPS);
+            //Make sure list and rover pin are instantiated
+            if (pushPinCollection == null)
+            {
+                pushPinCollection = new ObservableCollection<Pushpin>();
+            }          
+           
+
+            setRoverPinLocation();         
+        }
+
+        /// <summary>
+        /// This is a bit of a hack but it's reliable
+        /// </summary>
+        private void setRoverPinLocation()
+        {
+            bool messageReceived = false;
+            var startTime = DateTime.UtcNow;
+
+            roverCoordinates = new GPSCoordinates();
+            roverPin = new Pushpin();
+
+            while (roverCoordinates.Location == null && (DateTime.UtcNow - startTime < TimeSpan.FromSeconds(2)))
+            {
+                //loop until the rover coordinates have been received, timeout after 2 seconds
+                messageReceived = true;
+            }
+
+            //if the message was successfully received, bind the location
+            if (messageReceived)
+            {
+                roverPin.Location = roverCoordinates.Location;
+
+                formatRoverPin();
+            }
+        }
+
+        /// <summary>
+        /// Change the style of the rover pin
+        /// </summary>
+        private void formatRoverPin()
+        {
+            ToolTip tt = new ToolTip();
+            tt.Content = "Rover";
+            roverPin.ToolTip = tt;
+
+            roverPin.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 255));
+        }
+
+        /// <summary>
+        /// Change the style of the target pins
+        /// </summary>
+        private void formatTargetPin(Pushpin targetPin)
+        {
+            ToolTip tt = new ToolTip();
+
+            //Title is not mandatory
+            if (!string.IsNullOrWhiteSpace(targetTitleString))
+            {
+                tt.Content = targetTitleString + ": ";
+            }
+            else
+            {
+                //avoids null tooltip content
+                tt.Content = "";
+            }
+
+            tt.Content += targetPin.Location.Latitude + ", " + targetPin.Location.Longitude;
+            targetPin.ToolTip = tt;
+
+            targetPin.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 255, 0));
         }
 
         #endregion
 
+        #region Methods
+
+        /// <summary>
+        /// Decide whether or not to allow the command to execute
+        /// </summary>
+        private void determineCanAddTarget()
+        {
+            if (!string.IsNullOrWhiteSpace(longitudeString) && !string.IsNullOrWhiteSpace(latitudeString))
+            {
+                canAddTarget = true;
+            }
+            else
+            {
+                canAddTarget = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Updates the information in the details view
+        /// </summary>
+        public void updateTargetDetails()
+        {
+            foreach (var target in targetPins)
+            {
+                double targetLongitude = target.Location.Longitude;
+                double targetLatitude = target.Location.Latitude;
+
+                string targetLongitudeString = targetLongitude.ToString();
+                string targetLatitudeString = targetLatitude.ToString();
+
+                double distance = getDistance(roverPin.Location, target.Location);
+            }
+        }
+
+        /// <summary>
+        /// Get distance in kilometers between two locations
+        /// </summary>
+        /// <param name="latitude1"></param>
+        /// <param name="longitude1"></param>
+        /// <param name="latitude2"></param>
+        /// <param name="longitude2"></param>
+        /// <returns></returns>
+        private double getDistance(Location location1, Location location2)
+        {
+            double latitude1 = location1.Latitude;
+            double longitude1 = location1.Longitude;
+
+            double latitude2 = location2.Latitude;
+            double longitude2 = location2.Longitude;
+
+            double radiusOfEarth = 6371;
+            double lateralDistanceRadians = degreesToRadians(latitude2 - latitude1);
+            double longitudinalDistanceRadians = degreesToRadians(longitude2 - longitude1);
+            double a = Math.Sin(lateralDistanceRadians / 2) * Math.Sin(lateralDistanceRadians / 2) + Math.Cos(degreesToRadians(latitude1)) * Math.Cos(degreesToRadians(latitude2)) * Math.Sin(longitudinalDistanceRadians / 2) * Math.Sin(longitudinalDistanceRadians / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double distance = radiusOfEarth * c;
+            return distance;
+        }
+
+        /// <summary>
+        /// Convert degrees to radians
+        /// </summary>
+        /// <param name="degrees"></param>
+        /// <returns></returns>
+        private double degreesToRadians(double degrees)
+        {
+            return degrees * (Math.PI / 180);
+        }
+
+        /// <summary>
+        /// Add a target to the map (triggered by Add button)
+        /// </summary>
+        /// <param name="targetLocation"></param>
+        private void addTarget(Location targetLocation)
+        {
+            //Initialize target list
+            if (targetPins == null)
+            {
+                targetPins = new List<Pushpin>();
+            }
+
+            // The pushpin to add to the map.
+            Pushpin targetPin = new Pushpin();
+            targetPin.Location = targetLocation;
+            
+            //Style the target
+            formatTargetPin(targetPin);
+
+            targetPin.MouseRightButtonUp += targetPin_MouseRightButtonUp;
+
+            //Initialize target list
+            if (targetPins == null)
+            {
+                targetPins = new List<Pushpin>();
+            }
+            targetPins.Add(targetPin);
+
+            //Initialize the pushpin list
+            if (pushPinCollection == null)
+            {
+                pushPinCollection = new ObservableCollection<Pushpin>();
+            }
+            pushPinCollection.Add(targetPin);
+        }
+
+        /// <summary>
+        /// Right click event to remove target (best way to do this even though using commands seems to be more elegant)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void targetPin_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            removeTarget((Pushpin)sender);
+        }
+
+        /// <summary>
+        /// Remove target from map (triggered by right clocking on a pushpin)
+        /// </summary>
+        private void removeTarget(Pushpin targetToRemove)
+        {
+            //check that pushpin is actually a target and is in the map's collection of pins
+            if (pushPinCollection.Contains(targetToRemove) && targetPins.Contains(targetToRemove))
+            {
+                pushPinCollection.Remove(targetToRemove);
+                targetPins.Remove(targetToRemove);
+            }
+        }
+
+        /// <summary>
+        /// Data validation on user entered coordinates
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private bool validateCoordinateString(string coordinateString, out double coordinate)
+        {
+            bool result = (Double.TryParse(coordinateString, out coordinate));
+
+            //Allowed text = double format or empty string
+            if (!result && !(coordinateString == "") && !(coordinateString == "-"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
         
+        #endregion
+
+        #region Commands
+
+        public bool canAddTarget;
+        public RelayCommand _addTargetCommand;
+        public ICommand AddTargetCommand
+        {
+            get
+            {
+                if (_addTargetCommand == null)
+                {
+                    _addTargetCommand = new RelayCommand(param => this.ExecuteAddTargetCommand(), param => this.canAddTarget);
+                }
+                return _addTargetCommand;
+            }
+        }
+
+        /// <summary>
+        /// Add the target pin to target list
+        /// </summary>
+        private void ExecuteAddTargetCommand()
+        {           
+            Location targetLocation = new Location();
+
+            //latitude ranges from -90 to 90 
+            targetLocation.Latitude = latitude % 90;
+
+            //longitude ranges from -180 to 180
+            targetLocation.Longitude = longitude % 180;
+
+            //add the target to the map
+            addTarget(targetLocation);
+
+            //reset values of the textboxes
+            longitudeString = "";
+            latitudeString = "";
+            targetTitleString = "";
+
+            //reset the data values
+            longitude = 0;
+            latitude = 0;                        
+        }
+
+        public RelayCommand _removeTargetCommand;
+        public ICommand RemoveTargetCommand
+        {
+            get
+            {
+                if (_removeTargetCommand == null)
+                {
+                    _removeTargetCommand = new RelayCommand(param => this.ExecuteRemoveTargetCommand(), param => targetPins.Count > 0);
+                }
+                return _removeTargetCommand;
+            }
+        }
+
+        /// <summary>
+        /// Remove the target pin to target list
+        /// </summary>
+        private void ExecuteRemoveTargetCommand()
+        {
+            //removeTarget();
+        }
+
+        #endregion
     }
 
     #region Class Definitions
