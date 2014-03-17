a41759f1c3be9c1f84cc96a89bb88217cdf6f597
Pascal Guay <pascalg@gmail.com>
2014-03-16 21:50:38 -0400

    Remapped pan tilt values: 0 to 90 for tilt, 0 to 200 for pan speed. Integrated back the IMU, it works now because we changed for another one that has better hardware on it / filtering noise capabilities.

--- MarsRover/Microcontroller/Minibot-Microcontroller/MergedSystem/Camera.cpp
+++ MarsRover/Microcontroller/Minibot-Microcontroller/MergedSystem/Camera.cpp
@@ -11,8 +11,8 @@ Camera::Camera(int pinServoPan, int pinServoTilt,int camID)
   _pinServoTilt = pinServoTilt;
   _servoPan.attach(_pinServoPan);
   _servoTilt.attach(_pinServoTilt);
-  Pan(1521); // Stopping Pan
-  Tilt(1500); // Centering Tilt
+  Pan(100); // Stopping Pan
+  Tilt(45); // Centering Tilt
 }
 
 //Note that sending a 90 degree angle to the 360 continuous panning servo make it stop
@@ -22,7 +22,8 @@ void Camera::Pan(int angle)
   //Full stop 1521
   //For mapping functionality use
   //_servoPan.writeMicroseconds((int)map(angle,0, 90,1409,1633));
-  _servoPan.writeMicroseconds(angle);
+  _servoPan.writeMicroseconds((int)map(angle,0, 200,1429,1613));
+  //_servoPan.writeMicroseconds(angle);
   Serial.print("Camera Panned with speed: ");
   Serial.println(angle);
 }
@@ -31,7 +32,8 @@ void Camera::Tilt(int angle)
 {
   //For mapping functionality use
   //_servoTilt.writeMicroseconds((int)map(angle,0, 90,800,2200));
-  _servoTilt.writeMicroseconds(angle);
+  _servoTilt.writeMicroseconds((int)map(angle,90, 0,900,2100));
+  //_servoTilt.writeMicroseconds(angle);
   Serial.print("Camera Tilted with angle: ");
   Serial.println(angle);
 }
