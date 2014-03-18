d37652aaa0625a4c57d386a458d004dfa1b09d38
BBackerry <falah.salim@gmail.com>
2014-03-17 23:46:46 -0400

    Reworked Pinger to not use System.Net.Pinger anymore

--- MarsRover/MarsRover/Communication/Pinger.cs
+++ MarsRover/MarsRover/Communication/Pinger.cs
@@ -7,6 +7,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using NLog;
+using System.Net.Sockets;
+using System.Net;
 
 namespace MarsRover.Communication
 {
@@ -32,8 +34,8 @@ namespace MarsRover.Communication
             get { return RTT; }
             set
             {
-                if (RTTChanged != null) RTTChanged(value);
                 RTT = value;
+                if (RTTChanged != null) RTTChanged(value);
             }
         }
 
@@ -43,8 +45,8 @@ namespace MarsRover.Communication
             get { return connectedToRover; }
             set
             {
-                if (ConnectivityChanged != null) ConnectivityChanged(value);
                 connectedToRover = value;
+                if (ConnectivityChanged != null) ConnectivityChanged(value);
             }
         }
 
@@ -69,7 +71,9 @@ namespace MarsRover.Communication
 
         private Pinger()
         {
-            //StartPinging();
+            Thread t = new Thread(() => Ping());
+            t.IsBackground = true;
+            t.Start();
         }
 
         public static Pinger Instance
@@ -91,41 +95,106 @@ namespace MarsRover.Communication
             }
         }
 
-        private void StartPinging()
-        {
-            Thread t = new Thread(() => Ping());
-            t.IsBackground = true;
-            t.Start();
-        }
+        #endregion
+
+        #region Methods
 
         private void Ping()
         {
-            bool pingable = false;
-            Ping pinger = new Ping();
+            byte[] data = new byte[1024];
+            int recv;
+            Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(roverIPAddress), 0); //ICMP doesn't use port numbers
+            EndPoint ep = (EndPoint)iep;
+            ICMP packet = new ICMP();
+            packet.Type = 0x08;
+            packet.Code = 0x00;
+            packet.Checksum = 0;
+            Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes((short)1), 0, packet.Message, 2, 2);
+            data = Encoding.ASCII.GetBytes("ping");
+            Buffer.BlockCopy(data, 0, packet.Message, 4, data.Length);
+            packet.MessageSize = data.Length + 4;
+            int packetsize = packet.MessageSize + 4;
+            UInt16 chcksum = packet.getChecksum();
+            packet.Checksum = chcksum;
+            host.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 1000);
+
             while (true)
             {
+                DateTime sentAt = DateTime.Now;
+                DateTime recvdAt = sentAt;
+                host.SendTo(packet.getBytes(), packetsize, SocketFlags.None, iep);
                 try
                 {
-                    PingReply reply = pinger.Send(roverIPAddress);
-                    pingable = reply.Status == IPStatus.Success;
-                    PingRTT = reply.RoundtripTime;
-                }
-                catch (PingException) { }
-
-                if (pingable)
-                {
+                    data = new byte[1024];
+                    recv = host.ReceiveFrom(data, ref ep);
+                    recvdAt = DateTime.Now;
+                    ICMP response = new ICMP(data, recv);
+                    DateTime recvd = DateTime.Now;
                     ConnectedToRover = true;
+                    PingRTT = (long)(recvdAt - sentAt).TotalMilliseconds;
                 }
-                else
+                catch (SocketException)
                 {
                     ConnectedToRover = false;
+                    PingRTT = 0;
                 }
-                Thread.Sleep(200);
+
+                Thread.Sleep(500);
             }
         }
 
         #endregion
 
+
+        //From "C# Network Programming" by Richard Blum
+        class ICMP
+        {
+            public byte Type;
+            public byte Code;
+            public UInt16 Checksum;
+            public int MessageSize;
+            public byte[] Message = new byte[1024];
+
+            public ICMP() { }
+
+            public ICMP(byte[] data, int size)
+            {
+                Type = data[20];
+                Code = data[21];
+                Checksum = BitConverter.ToUInt16(data, 22);
+                MessageSize = size - 24;
+                Buffer.BlockCopy(data, 24, Message, 0, MessageSize);
+            }
+
+            public byte[] getBytes()
+            {
+                byte[] data = new byte[MessageSize + 9];
+                Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+                Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, data, 1, 1);
+                Buffer.BlockCopy(BitConverter.GetBytes(Checksum), 0, data, 2, 2);
+                Buffer.BlockCopy(Message, 0, data, 4, MessageSize);
+                return data;
+            }
+
+            public UInt16 getChecksum()
+            {
+                UInt32 chcksm = 0;
+                byte[] data = getBytes();
+                int packetsize = MessageSize + 8;
+                int index = 0;
+
+                while (index < packetsize)
+                {
+                    chcksm += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                    index += 2;
+                }
+                chcksm = (chcksm >> 16) + (chcksm & 0xffff);
+                chcksm += (chcksm >> 16);
+                return (UInt16)(~chcksm);
+            }
+        }
 
 
     }
